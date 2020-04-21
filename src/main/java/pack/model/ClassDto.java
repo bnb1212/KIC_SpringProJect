a5c36452e0a9310491ede056ac9fcd5d946dcbe7
Byunggukson <50914614+Byunggukson@users.noreply.github.com>
2020-04-21 15:22:13 +0900

    1522 merge

--- src/main/java/pack/model/ClassDto.java
+++ src/main/java/pack/model/ClassDto.java
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-package pack.model;
-
-public class ClassDto {
-	private String class_no, class_name, class_info, class_price, class_category;
-	private int class_limit;
-	public String getClass_no() {
-		return class_no;
-	}
-	public void setClass_no(String class_no) {
-		this.class_no = class_no;
-	}
-	public String getClass_name() {
-		return class_name;
-	}
-	public void setClass_name(String class_name) {
-		this.class_name = class_name;
-	}
-	public String getClass_info() {
-		return class_info;
-	}
-	public void setClass_info(String class_info) {
-		this.class_info = class_info;
-	}
-	public String getClass_price() {
-		return class_price;
-	}
-	public void setClass_price(String class_price) {
-		this.class_price = class_price;
-	}
-	public String getClass_category() {
-		return class_category;
-	}
-	public void setClass_category(String class_category) {
-		this.class_category = class_category;
-	}
-	public int getClass_limit() {
-		return class_limit;
-	}
-	public void setClass_limit(int class_limit) {
-		this.class_limit = class_limit;
-	}
-	
-
-}
-=======
-
 package pack.model;
 
 public class ClassDto {
@@ -88,4 +41,3 @@ public class ClassDto {
 	}
 	
 }
->>>>>>> refs/heads/master
