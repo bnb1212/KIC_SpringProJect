02ee4bc4d2e4078ac08aa66a532bd75f20604d96
KITCOOP <bnb0409@gmail.com>
2020-04-21 19:45:33 +0900

    19:45

---
+++ src/main/java/pack/controller/MainController.java
@@ -0,0 +1,14 @@
+package pack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("main")
+	public String home(Model model) {
+		return "main";
+	}
+}
